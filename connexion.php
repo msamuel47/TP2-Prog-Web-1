f2d8b26fdd9bbb302c8708271735e937e0f4c7d8
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-03-01 15:30:12 -0500

    Messing with connexion.php and some css tweaking

--- connexion.php
+++ connexion.php
@@ -1,25 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Samuel
- * Date: 2017-03-01
- * Time: 2:53 PM
- */
-echo "
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin connect</title>
+    <link rel="stylesheet" href="CSS/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
 
-<div id=\"login\">  
-    <form action=\"validerUtilisateur.php\" method=\"post\">
-                        <table>
-                        <tr>
-                            <td><label>Nom d'utilisateur :</label></td>
-                            <td><input type=\"text\" name=\"username\"/></td>
-                        </tr>
-                        <tr>
-                            <td><label>Mot de passe :</label></td>
-                            <td> <input type=\"password\" name=\"mdp\" /></td>
-                        </tr>
-                        </table>                        
+</head>
+<body>
+<div id="login">
+    <form id="login" action="validerUtilisateur.php" method="post">
+        <table>
+            <tr>
+                <td><label>Nom d'utilisateur :</label></td>
+                <td><input type="text" name="username"/></td>
+            </tr>
+            <tr>
+                <td><label>Mot de passe :</label></td>
+                <td> <input type="password" name="mdp" /></td>
+            </tr>
+        </table>
     </form>
-</div>     
-                        ";
-?>
+</div>
+</body>
--- entete.php
+++ entete.php
@@ -4,7 +4,7 @@ function ShowHeader()
 {
     echo '	<div id="entete">
 			<div id="logo"></div>
-			<div id="login_space"><a id="loginlink" href="connexion.php"></a></div>
+			<div id="login_space"><p id="loginlink"><a  href="connexion.php">Connexion</a></p></div>
 			<div id="banner"></div>
 			
 			<div class="clear"></div>
