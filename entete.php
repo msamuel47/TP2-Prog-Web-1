07c2da9af6ebfeeacbe3452013f88eb4f0eba5dd
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-03-01 08:36:22 -0500

    Continue adding ShowHeader() method in each of the pages

--- entete.php
+++ entete.php
@@ -10,7 +10,7 @@ function ShowHeader()
 		<div id="menu">
 			<div class="linkitem"><a href="index.php">Acceuil</a></div>
 			<div class="linkitem"><a href="tournoisAVenir.php">Tournois à venir</a></div>
-			<div class="linkitem"><a href="4games.html">Top 4 jeux</a></div>
+			<div class="linkitem"><a href="recherche.php">Rechercher</a></div>
 			<div class="linkitem"><a href="player.html">Top 3 joueurs</a></div>
 			<div class="clear"></div>
 		</div>';
--- recherche.php
+++ recherche.php
@@ -1,5 +1,8 @@
 <!doctype html>
 <html lang="en">
+<?php
+include  "entete.php";
+?>
 <head>
     <meta charset="UTF-8">
     <title>Top 3 joueurs</title>
@@ -8,18 +11,9 @@
 </head>
 <body>
 <div id="container">
-    <div id="entete">
-        <img class="logoandbanner" src="img/logo.png" alt="TheLogo">
-        <img class="logoandbanner" src="img/banner.png" alt="TheBanner">
-        <div class="clear"></div>
-    </div>
-    <div id="menu">
-        <div class="linkitem"><a href="index.html">Acceuil</a></div>
-        <div class="linkitem"><a href="Evenement.html">Événement</a></div>
-        <div class="linkitem"><a href="4games.html">Top 4 jeux</a></div>
-        <div class="linkitem"><a href="player.html">Top 3 joueurs</a></div>
-        <div class="clear"></div>
-    </div>
+    <?php
+    ShowHeader();
+    ?>
     <div id="contenu">
 
 
