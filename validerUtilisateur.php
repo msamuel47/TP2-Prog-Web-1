2375921de651ebd0b582176da6a679f9b49c26fa
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-03-01 19:28:23 -0500

    Added session creation

--- validerUtilisateur.php
+++ validerUtilisateur.php
@@ -8,7 +8,33 @@
 
 if (!empty($_POST['username']) && !empty($_POST['mdp']))
 {
-    for
+    if (is_file('admin.txt'))
+    {
+        $adminValidation = fopen('admin.txt' , 'r');
+       $verification = file($adminValidation);
+       foreach($verification as $jaggedArray)
+       {
+           $userAndPassword = array(explode('|' , $jaggedArray));
+       }
+       foreach($userAndPassword as $user => $password)
+       {
+           if($user == $_POST['username'])
+           {
+                if($password == $_POST['mdp'])
+                {
+                    session_start();
+                    $_SESSION['user'] = $_POST['username'];
+                }
+           }
+           else
+               {
+
+               }
+       }
+
+
+
+    }
 }
 else
 {
