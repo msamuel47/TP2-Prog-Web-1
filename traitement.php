368ee1caa35ff6f99d25b0adf3bdf2998a267d36
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-03-05 18:37:35 -0500

    Will start shortly to mess with afficherTournois.php

--- traitement.php
+++ traitement.php
@@ -12,8 +12,10 @@ if (!empty($_POST['nomEvent']) && !empty($_POST['date']) && !empty($_POST['city'
     && !empty($_POST['maxPlayer']) && !empty($_POST['country']) && !empty($_POST['game'])
 ) {
     EnregistrerLaForm();
+    //Validation avancée pour verification des champs manquant
 } else {
-    //Erreur 1 seulement pour tester pour l'instant ne pas oublier l'ajout d'autre codes d'erreur
+
+    //Erreur 1 = des champs sont manquants veuillez les remplir ...
     header('Location:enregistrer.php?error=1');
 }
 
@@ -22,25 +24,26 @@ function EnregistrerLaForm()
     if (is_file("tournois.txt") == true) {
         //Ouverture du fichier en mode ecriture
         $fichier = fopen("tournois.txt", "a");
+        //Creation d'un tableau contenant les données entrées dans la form
         $tournoiAEnregistrer = array($_POST['nomEvent'], $_POST['date'], $_POST['city'], $_POST['city'], $_POST['country'], $_POST['game'], $_POST['maxPlayer']);
-        var_dump($tournoiAEnregistrer);
+        //Creation d'un tableau pour la création d'un timestamp
         $validerDate = explode('-', $_POST['date']);
-        // Valider la date ...
         $dateEnInt = array(intval($validerDate[0]), intval($validerDate[1]), intval($validerDate[2]));
+        //Création du timestamp en fonction de la date donnée
         $timestamp = mktime(0, 0, 0, $validerDate[1], $validerDate[2], $validerDate[0]);
+        //Conversion du timestamp en string pour l'inserstion du tournois dans le tableau
         $stringTimestamp = strval($timestamp);
-        //Debugging a retirer a la fin du projet
-        var_dump($dateEnInt);
-        var_dump($timestamp);
+
         //Inscription du tournois dans le fichier de texte
         $evenementFormate = "\r\n"  .$_POST['nomEvent'] . '|' . $stringTimestamp . '|' . $_POST['city'] . '|' . $_POST['country'] . '|' . $_POST['game'] . '|' . $_POST['maxPlayer'];
         var_dump($evenementFormate);
+        // Si l'écriture sur le fichier ne fonctionne pas ...
         if(fwrite($fichier , $evenementFormate) == true){
             //Code de succès 1 = l'événement à bel et bien été enregistré ...
             header('Location:enregistrer.php?succes=1');
         }
         else {
-            //Erreur 200 = probleme avec le fichier : ouvert en readOnly ? , le fichier est t'il en cour d'utilisation ?
+            //Erreur 200 = probleme avec le fichier : ouvert en readonly ? , le fichier est t'il en cour d'utilisation ?
             header('Location:enregistrer.php?error=200');
             fclose($fichier);
         }
