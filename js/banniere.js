69298e11bedcedee61b4f267df4172a66742f4be
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-03-06 15:25:46 -0500

    start working on banniere.js

--- js/banniere.js
+++ js/banniere.js
@@ -1,2 +1,21 @@
+var compteur = 0;
+var pictureNameTab= ["img/a.jpg", "img/b.jpg", "img/c.jpg",
+    "img/d.jpg", "img/e.jpg", "img/f.jpg" ,"img/g.jpg" ,"img/h.jpg"];
+var debutCompteur = Math.round(new Date().getTime()/1000);
 
+function nextPicture(interval) {
+
+
+
+    if(Math.round(new Date().getTime()/1000) == debutCompteur + interval){
+        compteur++;
+        
+        debutCompteur = Math.round(new Date().getTime()/1000);
+    }
+
+
+
+
+    document.getElementById("carousel").src = pictureNameTab[compteur];
+}
 
