779d2983f0a04d8e226c51442ea9ecb9943ce0ed
félix bergeron <felixbergeron0147@hotmail.com>
2017-03-06 20:59:38 +0100

    messing with recherche.php and traitementRecherche.php

--- recherche.php
+++ recherche.php
@@ -1,8 +1,6 @@
 <!doctype html>
 <html lang="en">
 <?php
-include  "resultatrecherche.php";
-include  "rechercher.php";
 include  "entete.php";
 include  "pieddepage.php";
 ?>
@@ -30,17 +28,11 @@ include  "pieddepage.php";
 			   <img id="defile" src="img/image1.jpg" width="100%" >
 			</div>
 			<div id="droite">
-				<form action="">
+				<form action="traitementRecherche.php" method="post">
+				 <table>
+				    
 					nom du tournoi        :<input type="text" name="nom"/>
-					</br>
-					 date:<input type="text" name="date"/>
-					</br>
-					emplacement      :<input type="text" name="emplacement"/>
-					</br>	
-					jeux<input type="text" name="jeux"/>
-					</br>
-					nombre de joueurs<input type="text" name="joueur"/>
-					</br>
+					</table>
 					<button type="submit" value="submit">submit</button>
 				</form>
 			</div>
--- traitementRecherche.php
+++ traitementRecherche.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Samuel
- * Date: 2017-03-06
- * Time: 2:20 PM
- */
+
+echo $_POST["nom"];
+if(is_file("tournois.txt")){
+	$tableauTournois = file('tournois.txt');
+	
+	$tournoisAVerifier = [];
+	foreach($tableauTournois as $index => $value){
+		$tournoisAVerifier[$index] = explode('|' , $value);
+	}
+	var_dump($tournoisAVerifier);
+	for($i = 0 ; $i < count($tournoisAVerifier) ; $i++){
+		if(tournoisAVerifier[$i][0] ==  $_POST['nom']){
+			header()
+		}
+	}
+}
+else{
+	
+}
+
+?>
